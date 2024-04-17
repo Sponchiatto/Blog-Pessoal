57a51abce9785be104c87e06b34dcb9e00096c31
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-04-17 19:37:21 -0300

    Página início parcialmente concluída.
    Preciso resolver o problemas das imagens dos posts.

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Banner from "componentes/Banner";
+import styles from "./Home.module.css";
+import posts from "json/posts.json";
+import Post from "componentes/Post";
 
 function Home() {
   return (
     <main>
       <Banner />
-      <h1>Olá mundo!</h1>
+      <ul className={styles.posts}>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Post post={post} />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
