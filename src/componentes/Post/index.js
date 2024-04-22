354e09982f49b4e3bfe3693cfb8fced68c48441a
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-04-22 16:14:40 -0300

    Colocada a secção outros posts.
    Primeiro usando métodos de array para filtrar os posts que serão mostrados.
    
    Segundo, uma função map desses posts recomendados que gerão o JSX de li com componente PostCard

--- src/componentes/Post/index.js
+++ src/componentes/Post/index.js
@@ -6,6 +6,8 @@ import "./Post.css";
 import NaoEncontrada from "paginas/NaoEncontrada";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import { Routes, Route } from "react-router-dom";
+import styles from "./Post.module.css";
+import PostCard from "componentes/PostCard";
 
 export default function Post() {
   const parametros = useParams();
@@ -15,6 +17,12 @@ export default function Post() {
   if (!post) {
     return <NaoEncontrada />;
   }
+
+  const postsRecomendados = posts
+    .filter((post) => post.id !== Number(parametros.id))
+    .sort((a, b) => b.id - a.id)
+    .slice(0, 4);
+
   return (
     <Routes>
       <Route path="*" element={<PaginaPadrao />}>
@@ -28,6 +36,16 @@ export default function Post() {
               <div className="post-markdown-container">
                 <ReactMarkdown>{post.texto}</ReactMarkdown>
               </div>
+              <h2 className={styles.tituloOutrosPosts}>
+                Outros Posts que podem te interessar:
+              </h2>
+              <ul className={styles.postsRecomendados}>
+                {postsRecomendados.map((post) => (
+                  <li key={post.id}>
+                    <PostCard post={post} />
+                  </li>
+                ))}
+              </ul>
             </PostModelo>
           }
         />
